dcc670e2c548810cb2fa03cc0413a471f57698f8
Jaziel96 <jazcov96@hotmail.com>
2022-07-29 23:54:22 +0000

    Carta aleatoria

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function aleatorioVal() {
+  let medios = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let puntas = ["♦", "♥", "♠", "♣"];
+  const cartas = [];
+  for (let a = 0; a < medios.length; a++) {
+    for (let b = 0; b < puntas.length; b++) {
+      const punta = puntas[b];
+      const medio = medios[a];
+      cartas.push({ medio, punta });
+    }
+  }
+  return cartas;
+}
+function random(cartas) {
+  const aleatorio = Math.floor(Math.random() * 51);
+  const valor = cartas[aleatorio].medio;
+  const forma = cartas[aleatorio].punta;
+  const carta = document.getElementById("carta");
+  carta.innerHTML =
+    forma == "♠" || forma == "♣"
+      ? `<span class="figura-arriba">${forma}</span><span class="numero">${valor}</span><span class="figura-abajo">${forma}</span>`
+      : `<span class="figura-arriba-rojo">${forma}</span><span class="numero-rojo">${valor}</span><span class="figura-abajo-rojo">${forma}</span>`;
+}
+const cartas = aleatorioVal();
+random(cartas);
